62eb8b096ced8f6782eaad5b9039ebda1628d1cd
zzycwmx <53326997+zzycwmx@users.noreply.github.com>
2020-07-06 10:55:36 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -176,13 +176,13 @@ benchinit() {
 	chmod a+rx ip_info.py
 
 	# install fast.com-cli
-# 	if  [ ! -e 'fast_com.py' ]; then
-# 		echo " Installing Fast.com-cli ..."
-# 		wget --no-check-certificate https://raw.githubusercontent.com/sanderjo/fast.com/master/fast_com.py > /dev/null 2>&1
-# 		wget --no-check-certificate https://raw.githubusercontent.com/sanderjo/fast.com/master/fast_com_example_usage.py > /dev/null 2>&1
-# 	fi
-# 	chmod a+rx fast_com.py
-# 	chmod a+rx fast_com_example_usage.py
+#	if  [ ! -e 'fast_com.py' ]; then
+#		echo " Installing Fast.com-cli ..."
+#		wget --no-check-certificate https://raw.githubusercontent.com/sanderjo/fast.com/master/fast_com.py > /dev/null 2>&1
+#		wget --no-check-certificate https://raw.githubusercontent.com/sanderjo/fast.com/master/fast_com_example_usage.py > /dev/null 2>&1
+#	fi
+#	chmod a+rx fast_com.py
+#	chmod a+rx fast_com_example_usage.py
 
 	sleep 2
 
