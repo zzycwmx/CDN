8b599e8cea41faa52aed0e66ec5e75878004165a
zzycwmx <53326997+zzycwmx@users.noreply.github.com>
2020-07-06 08:26:47 +0800

    Update ip_info.py

--- ip_info.py
+++ ip_info.py
@@ -2,11 +2,14 @@ import sys
 import urllib2
 import json
 url = "http://whois.pconline.com.cn/ipJson.jsp?json=true"
+def getAddr():
+    try:
+        r = urllib2.open(url)
+        j = json.loads(r.read())
+        return j["addr"]  
+    except:
+        return "None"
+    
 if __name__ == '__main__':
     if(len(sys.argv)>=2 and str(sys.argv[1])=="addr"):
-        try:
-            r = urllib2.open(url)
-            j = json.loads(r.read())
-            return j["addr"]
-        except:
-            return "None"
+        getAddr()
