bd87974cedf6ad32616774d15df3167d45d29b51
zzycwmx <53326997+zzycwmx@users.noreply.github.com>
2020-07-06 09:55:46 +0800

    Update ip_info.py

--- ip_info.py
+++ ip_info.py
@@ -7,22 +7,19 @@ import json
 reload(sys)
 sys.setdefaultencoding('utf-8')
 def unicode_convert(input_data):
-    if isinstance(input_data, dict):
-        return {unicode_convert(key): unicode_convert(value) for key, value in input_data.iteritems()}
-    elif isinstance(input_data, list):
-        return [unicode_convert(element) for element in input_data]
-    elif isinstance(input_data, unicode):
-        return input_data.encode('utf-8')
-    else:
-        return input_data
+    if isinstance(input_data, dict):
+        return {unicode_convert(key): unicode_convert(value) for key, value in input_data.iteritems()}
+    elif isinstance(input_data, list):
+        return [unicode_convert(element) for element in input_data]
+    elif isinstance(input_data, unicode):
+        return input_data.encode('utf-8')
+    else:
+        return input_data
 def getAddr():
-    try:
         r = urllib2.urlopen(r'http://whois.pconline.com.cn/ipJson.jsp?json=true')
-        j = json.loads(unicode_convert(r.read()))
-        print j["addr".encode('utf-8')]  
-    except Exception as e:
-        print e
-    
+        j = unicode_convert(json.loads(r.read(),encoding='GBK'))
+        print j["addr".encode('utf-8')]
+
 if __name__ == '__main__':
     if(len(sys.argv)>=2 and str(sys.argv[1])=="addr"):
         getAddr()
