e87affd14538cd21a1dacfa57fb93ba7652b4d3a
zzycwmx <53326997+zzycwmx@users.noreply.github.com>
2020-08-14 17:01:05 +0800

    Update superbench2.sh

--- superbench2.sh
+++ superbench2.sh
@@ -719,7 +719,7 @@ print_intro() {
 	printf " Mode  : \e${GREEN}%s\e${PLAIN}    Version : \e${GREEN}%s${PLAIN}\n" $mode_name 1.1.6 | tee -a $log
 	printf ' Usage : wget -qO- git.io/superbench.sh | bash\n' | tee -a $log
 	printf ' Fixed bugs version usage : wget -qO- https://git.io/Jvh0J | bash\n' | tee -a $log
-	printf ' 添加更多节点版本： wget -qO- git.io/vpstest | bash \n' | tee -a $log
+	printf ' 添加更多节点版本: wget -qO- git.io/vpsvps | bash \n' | tee -a $log
 	printf ' Fixed version 3\n' | tee -a $log
 }
 
